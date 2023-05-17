abcda84fd7e12d36b8009c03a553c4b8cb38528e
BriliyanLuthfiTriandri <briliyanlt@gmail.com>
2023-05-17 12:26:09 +0700

    mendefinisikan member function constructor

--- destructor/destructor.cpp
+++ destructor/destructor.cpp
@@ -18,30 +18,3 @@ angka::angka(int i) { //Constructor
 	isiData();
 }
 
-angka::~angka() { //Destructor
-	cout << endl;
-	cetakData();
-	delete[]arr;
-	cout << "Alamat Array Sudah Dilepaskan" << endl;
-}
-
-void angka::cetakData() {
-	for (int i = 1; i <= panjang; i++) {
-		cout << i << " = " << arr[i] << endl;
-	}
-}
-
-void angka::isiData() {
-	for (int i = 1; i <= panjang; i++) {
-		cout << i << " = "; cin >> arr[i];
-	}
-	cout << endl;
-}
-
-int main() {
-	angka belajarcpp(3); //constructor dipanggil
-	angka* ptrBelajarcpp = new angka(5); // Constructor dipanggil
-	delete ptrBelajarcpp; //Destructor dipanggil
-
-	return 0;
-}
